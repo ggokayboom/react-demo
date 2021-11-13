6b0c03790de8e5a8fd73d97147b76c495d0f5147
shaoxianxin <1282208632@qq.com>
2021-11-13 21:50:23 +0800

    add hello

--- src/App.js
+++ src/App.js
@@ -1,2 +1,15 @@
-import('react')
-import('react-dom')
+//创建外壳组件app
+//这一块的{Component}并不是解构复制
+import React, {Component} from "react"
+import Hello from "./Hello";
+//创建并暴露App组件
+export default class App extends Component{
+
+    render() {
+        return (
+            <div>
+                <Hello/>
+            </div>
+        )
+    }
+}
