70cff7fd1c9ca9c7ce9ef6afb1e93c176d5c1564
shaoxianxin <1282208632@qq.com>
2021-11-14 21:42:41 +0800

    add ajax demo

---
+++ ajax_src/App.jsx
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import axios from 'axios'
+
+
+class App extends Component {
+    getStudentData = ()=>{
+        axios.get('http://localhost:3000/api1/students').then(
+            response=>{
+                console.log('success', response.data)
+            },
+            error=>{
+                console.log('error', error.data)
+            }
+        )
+    }
+
+    getCarData = ()=>{
+        axios.get('http://localhost:3000/api2/cars').then(
+            response=>{
+                console.log('success', response.data)
+            },
+            error=>{
+                console.log('error', error.data)
+            }
+        )
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.getStudentData}>点我获取学生数据</button>
+                <button onClick={this.getCarData}>点我获取汽车数据</button>
+            </div>
+        );
+    }
+}
+
+export default App;
